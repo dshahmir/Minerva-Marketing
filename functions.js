1fa28904e822bbcf22a21820a1de4b6f3866093e
dshahmir <shahmirx5@gmail.com>
2023-06-21 15:14:37 +0500

    First commit

---
+++ functions.js
@@ -0,0 +1,13 @@
+
+
+function tap(){
+    let drop =document.querySelectorAll('drop');
+    if(drop.style.display = 'block'){
+        drop.style.display = 'none';
+    }
+    else{
+        drop.style.display = 'block';
+    }
+    
+    console.log("handle click happpens")
+}
