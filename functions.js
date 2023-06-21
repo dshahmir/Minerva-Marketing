242f999a457efca3bbe6dadba59a887e3a599dfa
Shahmir Khan <96733208+dshahmir@users.noreply.github.com>
2023-06-21 15:17:27 +0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,13 +1,2 @@
 
 
-function tap(){
-    let drop =document.querySelectorAll('drop');
-    if(drop.style.display = 'block'){
-        drop.style.display = 'none';
-    }
-    else{
-        drop.style.display = 'block';
-    }
-    
-    console.log("handle click happpens")
-}
